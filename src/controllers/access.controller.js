9cecec882aec80a74e045f1d8afdde464c6a2046
thientantran <thientantm@gmail.com>
2024-07-30 21:02:11 +0700

    Success Handling

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { CREATED } = require("../core/success.response")
 const AccessService = require("../services/access.service")
 
 class AccessController {
@@ -9,7 +10,14 @@ class AccessController {
             const {name, email, password} = req.body
             console.log(name, email, password)
             const result = await AccessService.signUp({name, email, password})
-            return res.status(201).json(result)
+            // return res.status(201).json(result)
+            new CREATED({
+                message: "Shop registerd successfully",
+                metadata: result,
+                options: {
+                    limit:10
+                }
+            }).send(res)
 
     // } catch (error) {
     //     next(error)
