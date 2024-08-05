a8ea2ffe2e8ee91839b42aaa10412198bef8987c
thientantran <thientantm@gmail.com>
2024-08-05 21:00:11 +0700

    Product DB design
    Dự án cần các sản phẩm, nhưng thuộc tính có thể khác nhau, nhưng ko nên thiết kế mỗi loại sản phẩm mỗi collection, mà nên làm chung, và các bảng thuộc tính nhỏ thì tách ra

---
+++ src/models/product.model.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const {model, Schema} = require("mongoose") 
+
+const DOCUMENT_NAME = 'Product'
+const CONNECTION_NAME = 'Products'
+
+var productSchema = new Schema({
+    product_name: {
+        type: String,
+        required: true
+    },
+    product_thumb: {
+        type: String,
+        required: true
+    },
+    product_price: {
+        type: Number,
+        required: true
+    },
+    product_description: {
+        type: String
+    },
+    product_quantity : {
+        type: Number,
+        required: true
+    },
+    product_type : {
+        type: String,
+        required: true,
+        enum: ["Electronics", "Clothing", "Books", "Home & Kitchen", "Beauty & Health", "Sports & Outdoors", "Toys & Games", "Others"]
+    },
+    product_shop: {
+        type: Schema.Types.ObjectId,
+        ref: 'Shop'
+    },
+    product_attributes :{
+        type: Schema.Types.Mixed,
+        required: true
+    }
+},{
+    collection: CONNECTION_NAME,
+    timestamps: true
+})
+
+// define the product type = clothing
+const clothingSchema = new Schema({
+    brand: {
+        type: String,
+        required: true
+    },
+    size: String,
+    material : String,
+},{
+    collection: "Clothes",
+    timestamps: true
+})
+
+// define the product type = electronics
+const electronicSchema = new Schema({
+    brand: {type: String, required: true},
+    model: String,
+    color: String,
+},
+{
+    collection: "Clectronics",
+    timestamps: true
+})
+module.exports = {
+    product : model(DOCUMENT_NAME, productSchema),
+    electronic: model("Electronic", electronicSchema),
+    clothing: model("Clothing", clothingSchema)
+}
